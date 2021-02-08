6c4e06d019be5aa33f49bb8403f6f29ad290314d
MHYNW <phdcarrot@naver.com>
2021-02-08 16:47:05 +0900

    first commit

---
+++ examples/opencv_example_001.py
@@ -0,0 +1,61 @@
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
+
+###############################################
+##      Open CV and Numpy integration        ##
+###############################################
+
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+# 640 x 480, 30fps
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+''' RGB Image
+config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+'''
+
+# Start streaming
+pipeline.start(config)
+
+try:
+    while True:
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        # color_frame = frames.get_color_frame()
+        '''
+        if not depth_frame or not color_frame:
+            continue
+        '''
+        if not depth_frame:
+            continue
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        # color_image = np.asanyarray(color_frame.get_data())
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        # Stack both images horizontally
+        #images = np.hstack((color_image, depth_colormap))
+
+        # Show images
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.line(depth_colormap, (310, 240), (330, 240), (0, 0, 200), 4)
+        cv2.line(depth_colormap, (320, 230), (320, 250), (0, 0, 200), 4)
+        cv2.imshow('RealSense', depth_colormap)
+        cv2.waitKey(1)
+        # print("depth: {0}, distance: {1}".format(depth_image(320, 240), depth_frame.get_distance(320,240)))
+        print("distnace: {}".format(depth_frame.get_distance(320, 240)))
+        print("depth: {}".format(depth_image[320,240]))
+
+finally:
+
+    # Stop streaming
+    pipeline.stop()
